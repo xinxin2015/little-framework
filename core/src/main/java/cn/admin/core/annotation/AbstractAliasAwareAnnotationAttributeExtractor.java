746f3126bcedf9e4158bdf7c6ad64a53813c6072
admin <1178919633@qq.com>
2019-01-21 22:05:35 +0800

    """

--- core/src/main/java/cn/admin/core/annotation/AbstractAliasAwareAnnotationAttributeExtractor.java
+++ core/src/main/java/cn/admin/core/annotation/AbstractAliasAwareAnnotationAttributeExtractor.java
@@ -1,6 +1,7 @@
 package cn.admin.core.annotation;
 
 import cn.admin.lang.Nullable;
+import cn.admin.util.Assert;
 
 import java.lang.annotation.Annotation;
 import java.util.List;
@@ -16,5 +17,15 @@ abstract class AbstractAliasAwareAnnotationAttributeExtractor<S> implements Anno
     private final S source;
 
     private final Map<String, List<String>> attributeAliasMap;
+
+    AbstractAliasAwareAnnotationAttributeExtractor(Class<? extends Annotation> annotationType,
+                                                   @Nullable Object annotatedElement,S source) {
+        Assert.notNull(annotationType, "annotationType must not be null");
+        Assert.notNull(source, "source must not be null");
+        this.annotationType = annotationType;
+        this.annotatedElement = annotatedElement;
+        this.source = source;
+        this.attributeAliasMap = null;//TODO
+    }
 
 }
---
+++ core/src/main/java/cn/admin/core/annotation/AnnotationUtils.java
@@ -0,0 +1,53 @@
+package cn.admin.core.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AnnotationUtils {
+
+    private static final Map<Class<? extends Annotation>,Map<String, List<String>>> attributeAliasesCache = null;//TODO
+
+
+    private static final class AnnotationCacheKey implements Comparable<AnnotationCacheKey> {
+
+        private final AnnotatedElement element;
+
+        private final Class<? extends Annotation> annotationType;
+
+        public AnnotationCacheKey(AnnotatedElement element,
+                                  Class<? extends Annotation> annotationType) {
+            this.element = element;
+            this.annotationType = annotationType;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) {
+                return true;
+            }
+            if (!(other instanceof AnnotationCacheKey)) {
+                return false;
+            }
+            AnnotationCacheKey otherKey = (AnnotationCacheKey) other;
+            return (this.element.equals(otherKey.element) && this.annotationType.equals(otherKey.annotationType));
+        }
+
+        @Override
+        public int hashCode() {
+            return (this.element.hashCode() * 29 + this.annotationType.hashCode());
+        }
+
+        @Override
+        public String toString() {
+            return "@" + this.annotationType + " on " + this.element;
+        }
+
+        @Override
+        public int compareTo(AnnotationCacheKey o) {
+            return 0;
+        }
+    }
+
+}
